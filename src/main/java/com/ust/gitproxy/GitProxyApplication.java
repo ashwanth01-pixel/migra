9d9ef8e06dd8f2ba9089af30820e8a1b024f07be
Ashwanthram A C <115695893+ashwanth01-pixel@users.noreply.github.com>
2025-10-28 10:29:25 +0530

    Initial commit: add src/main/java/com/ust/gitproxy/GitProxyApplication.java (migrated from ADO UST-Accounts1/USTP-PACE-BU-00_pace-git-lift/USTP-PACE-BU-00_pace-git-lift branch MeenakshyUST-patch-2)

---
+++ src/main/java/com/ust/gitproxy/GitProxyApplication.java
@@ -0,0 +1,13 @@
+package com.ust.gitproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProxyApplication.class, args);
+	}
+
+}
